8ce4d76f3d13aea560afdd032c8bb223900857b1
Aditya Setiono <winged.zach@gmail.com>
2016-12-04 21:33:50 +1100

    updated to es 2016

--- src/init/db.js
+++ src/init/db.js
@@ -3,23 +3,22 @@ import config from '../../config'
 
 const r = rethinkdbdash(config.rethinkdb)
 
-const initDB = () => {
-    r.tableList().contains('user').run().then((tableExists) => {
-        if (!tableExists) {
-            r.tableCreate('user')
-            .run()
-            .then((result) => {initUser()})
-        } else {
-            initUser()
-        }
-    })
+const initDB = async () => {
+    const tableExists = await r.tableList().contains('user').run().then()
+    if (!tableExists) {
+        await r.tableCreate('user').run().then()
+    }
+    await initUser()
 }
 
-const initUser = () => {
-    r.table('user')
-    .insert(r.http('https://api.github.com/users'))
-    .run()
-    .then(console.log)
+const initUser = async () => {
+    const count = await r.table('user').count().run().then()
+    if (count < 30) {
+        await r.table('user')
+        .insert(await r.http('https://api.github.com/users').run().then())
+        .run()
+        .then()
+    }
 }
 
 export default initDB
--- src/routes/users.js
+++ src/routes/users.js
@@ -7,8 +7,38 @@ let router = express.Router()
 /* GET users listing. */
 router.get('/', (req, res, next) => {
     r.table('user')
-        .run()
-        .then((result) => res.json(result))
+    .run()
+    .then((result) => res.json(result))
+})
+
+router.get('/:id', (req, res, next) => {
+    r.table('user')
+    .get(+req.params.id)
+    .run()
+    .then((result) => res.json(result))
+})
+
+router.post('/', (req, res, next) => {
+    r.table('user')
+    .insert(req.body)
+    .run()
+    .then((result) => res.json(result))
+})
+
+router.put('/:id', (req, res, next) => {
+    r.table('user')
+    .get(+req.params.id)
+    .update(req.body, {returnChanges: true})
+    .run()
+    .then((result) => res.json(result))
+})
+
+router.delete('/:id', (req, res, next) => {
+    r.table('user')
+    .get(+req.params.id)
+    .delete()
+    .run()
+    .then((result) => res.json(result))
 })
 
 export default router
